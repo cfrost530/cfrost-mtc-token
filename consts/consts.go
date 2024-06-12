10c29113ac962ca5c04806ceab0beabf35ef10a7
cfrost530 <kbenny530@gmail.com>
2024-06-12 15:52:23 +0000

    cfrost commit

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "CFROSTPERCHAIN"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "CFROST"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "CFRST"
 )
 
 var ID ids.ID
